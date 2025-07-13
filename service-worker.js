ba73295bd2335046644de88d486817c7773259ce
SpongesLost <thespongeisop@gmail.com>
2025-07-13 02:58:12 +0200

    test

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,8 @@
+// Last updated: 2025-07-13T00:58:03.420Z
 // Last updated: 2025-07-13T00:37:31.635Z
 // Last updated: 2025-07-13T00:24:50.599Z
 // Last updated: 2025-07-13T12:45:00.000Z
-const CACHE_VERSION = 9;
+const CACHE_VERSION = 10;
 const CACHE_NAME = `pwa-cache-v${CACHE_VERSION}`;
 const urlsToCache = [
   '/',
